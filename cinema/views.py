1314b2fd8106bd6dcd1c89b890c3896f669fe57f
Yatsenko Dmytro <sonituensem@gmail.com>
2026-09-23 12:35:15 +0300

    Allow authenticated users to manage sessions

--- cinema/views.py
+++ cinema/views.py
@@ -107,7 +107,7 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         )
     )
     serializer_class = MovieSessionSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         date = self.request.query_params.get("date")
